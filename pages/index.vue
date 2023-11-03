0c2f65813f64348f836826162a050d60a0ddb1b4
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-11-03 16:47:00 +0100

    dependencies + styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <script setup></script>
 
 <template>
-	<div></div>
+	<div>
+		<h1>Same sample text for check</h1>
+		<h2>Same sample text for check</h2>
+		<h3>Same sample text for check</h3>
+		<h4>Same sample text for check</h4>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime
+			dolores repellat incidunt assumenda nulla pariatur temporibus,
+			necessitatibus repudiandae. Expedita blanditiis deleniti quae natus
+			maiores esse ratione adipisci quis consectetur enim!
+		</p>
+	</div>
 </template>
 
 <style></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                "wisp": ["Wisp", "system-ui", "sans-serif"]
+            },
+
+            fontSize: {
+                "fsm-txt": "18px",
+                "fsd-txt": "20px",
+                "fsm-h1": "80px",
+                "fsd-h1": "160px",
+                "fsm-h2": "38px",
+                "fsd-h2": "45px",
+                "fsm-h3": "24px",
+                "fsd-h3": "30px",
+                "fsm-h4": "17px",
+                "fsd-h4": "20px",
+                "fsm-link": "19px",
+                "fsd-link": "22px",
+                "fsm-btn": "12px",
+                "fsd-btn": "12px",
+                "fsm-note": "10px",
+                "fsd-note": "12px",
+            },
+            colors: {
+                "clr-bg": " #140f1f",
+                "clr-text": " #e9e2d0",
+                "clr-primary": " #28adc8",
+                "clr-input": " #13162a",
+            }
+        },
+    },
+    plugins: [],
 }
 
