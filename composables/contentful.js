773e90007436cf9433fc3434e877c74c319930f5
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-11-07 12:57:13 +0100

    up

--- composables/contentful.js
+++ composables/contentful.js
@@ -3,7 +3,6 @@ import * as contentful from 'contentful'
 export const useContentful = () => {
 
     const config = useRuntimeConfig()
-
     const client = contentful.createClient({
         space: config.public.space,
         accessToken: config.public.accessToken
